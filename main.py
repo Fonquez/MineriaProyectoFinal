a4c27fbbbea732a3ad676c1e96d0b0e0bd5cebc3
alimedi3 <53917383+alimedi3@users.noreply.github.com>
2021-01-25 20:54:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import pandas as pd
 
 data = pd.read_csv("pokemon.csv")
 st.write(data)
+
+import matplotlib as plt
+import seaborn as sns
+
+plot = sns.histplopt(data,x=data.base_total,y=data.is_legendary)
+
+st.write(plot)
