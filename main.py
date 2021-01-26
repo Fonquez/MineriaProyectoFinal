a699bdd36f69ea6c5c85ef9fa35b9444dfaa6900
alimedi3 <nikolai.garcia.98@gmail.com>
2021-01-25 21:39:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("pokemon.csv")
-st.write(data)
+poke = pd.read_csv("pokemon.csv")
+st.write(poke)
 
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-plot = px.histogram(data,x="base_total",y="is_legendary")
+no_leg = poke[(poke['is_legendary'] == 0)]
+leg = poke[(poke['is_legendary'] == 1)]
 
-st.plotly_chart(plot)
+fig, ax = plt.subplots()
+ax.hist(no_leg['base_total'], 10, None, ec='red', fc='none', lw=1.5, histtype='step', label='no legendary')
+ax.hist(leg['base_total'], 10, None, ec='green', fc='none', lw=1.5, histtype='step', label='legendary')
+ax.legend(loc='upper left')
+plt.xlabel("base_total")
+plt.ylabel("count")
+plt.show()
+
+st.pyplot(fig)
